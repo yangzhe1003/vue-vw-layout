d1cd5c25866c26aa3d458145d611c24549af2f9e
yangweizhe <yangweizhe@mifengkong.cn>
2018-12-10 15:09:41 +0800

    fix:test

---
+++ src/components/Test.vue
@@ -0,0 +1,27 @@
+
+<template>
+  <div class="test">
+    {{msg}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Test",
+  data() {
+    return {
+      msg: "hello viewport"
+    } 
+  }
+};
+</script>
+
+
+<style>
+.test {
+  width: 750px;
+  height: 160px;
+  background-color: skyblue;
+  font-size: 36px;
+}
+</style>
